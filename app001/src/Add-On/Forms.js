1e1ed5bfe302dad636cdd4b607d7d5eab9770279
Sukumar Pinjala <sukumar.pinjala@gmail.com>
2022-12-03 21:56:14 +0530

    Login Fields are entered

---
+++ app001/src/Add-On/Forms.js
@@ -0,0 +1,11 @@
+export const Form1 = () => {
+    return (
+        <div>
+            <form>
+                <h1> Welcome to the Page </h1>
+                <label> Username </label> <input type="text" />
+                <label> Password </label> <input type="password" />
+            </form>
+        </div>
+    )
+}
