3fddaa1c383d84892e35119af8d71fe39b1e50f4
Sukumar Pinjala <sukumar.pinjala@gmail.com>
2022-12-03 22:28:03 +0530

    Submit button Added

--- app001/src/Add-On/Forms.js
+++ app001/src/Add-On/Forms.js
@@ -1,13 +1,16 @@
+import {useState} from 'react'
+
 export const Form1 = () => {
+    const [data, setData] = useState("Hello")
+    
     return (
         <div>
-            <form>
                 <h1> Welcome to the Page </h1>
                 <label> Username </label> <input type="text" />
                 <label> Password </label> <input type="password" />
                 <h2> This is End of the Page </h2>
-                <h2> Hello World </h2>
-            </form>
+                <h2> {data} World </h2>
+                <button onClick={()=>setData("Superb")}> Submit </button>
         </div>
     )
 }
